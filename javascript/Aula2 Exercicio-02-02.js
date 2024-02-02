575f59a74fae496b5556b43052e81f1d7e7f0128
Carlos Vinícius Pereira <157232610+Cvinicius-7@users.noreply.github.com>
2024-02-02 23:18:44 +0000

    Add Input with parseFloat Method

--- javascript/Aula2 Exercicio-02-02.js
+++ javascript/Aula2 Exercicio-02-02.js
@@ -1,3 +1,8 @@
 nomeFunc = prompt("Qual é o nome do funconário?");
 anoContrato = prompt("Qual o ano de contratação dele?");
 anoAtual = new Date().getFullYear();
+anoEmpre= anoAtual - anoContrato;
+
+salBruto = parseFloat(prompt("Qual é o salário Bruto dele?"));
+gratifica = parseFloat(prompt("Qual são as bonificações dele?"));
+descontos = parseFloat(prompt("Qual são os descontos dele?"));
