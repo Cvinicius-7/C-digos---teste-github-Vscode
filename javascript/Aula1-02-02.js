f8b1c9df50b909b189fa7bd7b3670ee7c2d18b86
Carlos Vinícius Pereira <157232610+Cvinicius-7@users.noreply.github.com>
2024-02-02 22:15:48 +0000

    Add object to add acual year, and input birth year to create age.

--- javascript/Aula1-02-02.js
+++ javascript/Aula1-02-02.js
@@ -1,4 +1,7 @@
-idade = 19;
+anoAtual= new Date().getFullYear();
+anoNascimento = prompt("Digite Seu ano de Nascimento: ");
+idade = anoAtual - anoNascimento;
 nome = "Carlos";
-mensagem = "Sou " + nome + " E  tenho " + idade + " anos"
+mensagem = "Sou " + nome + " E  tenho " + idade + " anos";
 console.log(mensagem);
+alert(mensagem);
