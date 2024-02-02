32e19cceb7e7503fba5f896aff9765b0c0907b6e
Carlos Vinícius Pereira <157232610+Cvinicius-7@users.noreply.github.com>
2024-02-02 23:20:25 +0000

    Add Problem lógic and Console log/alert function

--- javascript/Aula2 Exercicio-02-02.js
+++ javascript/Aula2 Exercicio-02-02.js
@@ -2,7 +2,12 @@ nomeFunc = prompt("Qual é o nome do funconário?");
 anoContrato = prompt("Qual o ano de contratação dele?");
 anoAtual = new Date().getFullYear();
 anoEmpre= anoAtual - anoContrato;
-
 salBruto = parseFloat(prompt("Qual é o salário Bruto dele?"));
 gratifica = parseFloat(prompt("Qual são as bonificações dele?"));
 descontos = parseFloat(prompt("Qual são os descontos dele?"));
+
+salLiq = (salBruto + gratifica - descontos)*1.20 + anoEmpre *150;
+mensagem = "O funcionario " + nomeFunc + " recebe um salário líquido de R$"+salLiq;
+
+console.log(mensagem);
+alert(mensagem)
