0e89b229c8c88af73364c735e76760a09587bdd2
Carlos Vinícius Pereira <157232610+Cvinicius-7@users.noreply.github.com>
2024-02-02 23:16:31 +0000

    Change file name

---
+++ javascript/Aula2-02-02.js
@@ -0,0 +1,7 @@
+anoAtual= new Date().getFullYear();
+anoNascimento = prompt("Digite Seu ano de Nascimento: ");
+idade = anoAtual - anoNascimento;
+nome = "Carlos";
+mensagem = "Sou " + nome + " E  tenho " + idade + " anos";
+console.log(mensagem);
+alert(mensagem);
