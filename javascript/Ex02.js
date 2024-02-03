625a4d345355f14b7ce6adf27022e77b7a78989b
Carlos Vinícius Pereira <157232610+Cvinicius-7@users.noreply.github.com>
2024-02-03 10:40:32 -0300

    add first lcgic

--- javascript/Ex02.js
+++ javascript/Ex02.js
@@ -1,3 +1,6 @@
 nome = prompt('Digite seu nome');
 altura = parseFloat(prompt('Qual a sua altura?'));
 peso = parseFloat(prompt('Qual é o seu peso?'));
+imc = peso/(altura*altura);
+imcTot= imc.toFixed(1);
+cond = 'normal' 
