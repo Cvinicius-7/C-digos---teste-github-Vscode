f0845faddbcd9fb1992fc038c50018f2c9f9dbfe
Carlos Vinícius Pereira <157232610+Cvinicius-7@users.noreply.github.com>
2024-02-03 10:41:48 -0300

    add if case

--- javascript/Ex02.js
+++ javascript/Ex02.js
@@ -4,3 +4,21 @@ peso = parseFloat(prompt('Qual é o seu peso?'));
 imc = peso/(altura*altura);
 imcTot= imc.toFixed(1);
 cond = 'normal' 
+if(imcTot>40.0){
+    cond = 'Obesidade Grau 3'
+}
+if(imcTot>35.0 && imcTot<39.9){
+    cond = 'Obesidade Grau 2'
+}
+if(imcTot>30.0 && imcTot<34.9){
+    cond = 'Obesidade Grau 1'
+}
+if(imcTot>25.0 && imcTot<29.9){
+    cond = 'Sobrepeso'
+}
+if(imcTot>18.6 && imcTot<24.9){
+    cond = 'Normal'
+}
+if(imcTot<18.5){
+    cond = 'Abaixo do Normal'
+}
