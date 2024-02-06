f2f9b844ae860ee0e61977a5e13949b05e948c5a
Carlos Vinícius Pereira <p.carlosvini@gmail.com>
2024-02-05 22:37:42 -0300

    update

--- javascript/Ex03.js
+++ javascript/Ex03.js
@@ -1,5 +1,5 @@
 var n1 = Number(window.prompt('Digite Um número: '));
-var n2 = Number.(window.prompt('Digite Outro número: '));
+var n2 = Number(window.prompt('Digite Outro número: '));
 var s = n1 + n2;
 window.alert('A soma dos valores é ' + s); 
 //number + number = adição
