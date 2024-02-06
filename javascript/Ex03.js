aa89b598fcf3a0a0286cf78b82df7ee5841696ec
Carlos Vinícius Pereira <p.carlosvini@gmail.com>
2024-02-05 22:30:31 -0300

    Add logic and output

--- javascript/Ex03.js
+++ javascript/Ex03.js
@@ -1,2 +1,6 @@
 var n1 = Number.parseInt(window.prompt('Digite Um número: '));
 var n2 = Number.parseInt(window.prompt('Digite Outro número: '));
+var s = n1 + n2;
+window.alert('A soma dos valores é ' + s); 
+//number + number = adição
+//string +string = concatenação
