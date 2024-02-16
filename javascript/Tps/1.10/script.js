fd3fdb60a6fa1e56f866ad3b532f96eb4ddf8b35
Carlos Vinícius Pereira <p.carlosvini@gmail.com>
2024-02-15 23:05:25 -0300

    add tp 1.10

---
+++ javascript/Tps/1.10/script.js
@@ -0,0 +1,17 @@
+var primeiroNumero = prompt("Digite o Primeiro Numero");
+var segundoNumero = prompt("Digite o Segundo Numero");
+
+primeiroNumero = Number(primeiroNumero);
+segundoNumero = Number(segundoNumero);
+
+var adicao = primeiroNumero + segundoNumero;
+var subtracao = primeiroNumero - segundoNumero;
+var multiplicacao = primeiroNumero * segundoNumero;
+var divisao = primeiroNumero / segundoNumero;
+
+console.log("Adição: " + adicao);
+console.log("Subtração: " + subtracao);
+console.log("Multiplicação: " + multiplicacao);
+console.log("Divisão: " + divisao);
+
+// Seu código aqui...
