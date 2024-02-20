3e4ceb2b98e3a00a03c69de92ddc022cec8f4a46
Carlos Vinícius Pereira <p.carlosvini@gmail.com>
2024-02-19 22:03:48 -0300

    check and radio box JS

--- javascript/aula-6/scriptAula6.js
+++ javascript/aula-6/scriptAula6.js
@@ -3,12 +3,14 @@ function definir(){
     let valorSigno = Number(document.getElementById('signo').value);
 
     let valorTime = Number(document.getElementById('time').value);
+
+    let valorEscolaridade = Number(document.querySelector('input[id="escolaridade"]:checked').value)
     
     let qteSoftskill = Number(document.querySelectorAll('input[id="softskill"]:checked').length);
 
     let valorSoftskill = qteSoftskill*10;
 
-    let resultado = valorSigno + valorTime + valorSoftskill;
+    let resultado = valorSigno + valorTime + valorSoftskill + valorEscolaridade;
 
     document.getElementById("resultado").value = resultado;
 }
