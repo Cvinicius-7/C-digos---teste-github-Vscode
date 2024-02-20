d509324f94155ca4da54b7e8da33d627002741a0
Carlos Vinícius Pereira <p.carlosvini@gmail.com>
2024-02-19 21:49:02 -0300

    add checkbox javascript

--- javascript/aula-6/scriptAula6.js
+++ javascript/aula-6/scriptAula6.js
@@ -1,5 +1,14 @@
 function definir(){
 
+    let valorSigno = Number(document.getElementById('signo').value);
+
+    let valorTime = Number(document.getElementById('time').value);
     
-    
+    let qteSoftskill = Number(document.querySelectorAll('input[id="softskill"]:checked').length);
+
+    let valorSoftskill = qteSoftskill*10;
+
+    let resultado = valorSigno + valorTime + valorSoftskill;
+
+    document.getElementById("resultado")
 }
