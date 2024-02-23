fd71456281c019bd489ae5cd3e1da8ec57791faa
Carlos Vinícius Pereira <p.carlosvini@gmail.com>
2024-02-22 21:10:42 -0300

    add second example and comment test

--- javascript/Aula-7/script.js
+++ javascript/Aula-7/script.js
@@ -1,7 +1,15 @@
-let salario = 1000;
+let salario = 1000; //rico(>=1000), pobre(<500) e classe média(>=500 e <1000)
 
 if (salario >= 1000) {
   alert("Rico");
-} else {
+} else if (salario <= 500) {
   alert("Pobre");
+}else {
+  alert("Classe média");
 }
+ //casos de teste:
+ //salario = 1000, esperado: Rico
+ //salario = 500, esperado: Classe média
+ //salario = 0, esperado: Pobre
+ //salario = 499, esperado: Pobre
+ //salario = 999, esperado: Classe média
