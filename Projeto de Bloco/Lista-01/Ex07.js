f7d2f78d6a01cc9e17233e343b5d0f913d926f9d
Carlos Vinícius Pereira <157232610+Cvinicius-7@users.noreply.github.com>
2024-02-25 20:37:40 +0000

    Add ex 07 resolution

---
+++ Projeto de Bloco/Lista-01/Ex07.js
@@ -0,0 +1,11 @@
+/*Crie um algoritmo que calcule a gorjeta a ser paga de uma conta de restaurante. 
+A gorjeta é calculada como sendo 10% do valor da conta, que deve ser informado pelo usuário.*/
+
+let valConta = parseFloat(prompt("digite o valor da conta"));
+
+let gorjeta= valConta* 10/100;
+let contaFinal = valConta +gorjeta;
+
+alert("o valor da gorjeta será de: " + gorjeta +" Reais o valor final da conta será de: " + contaFinal + " Reais");
+console.log("o valor da gorjeta será de: " + gorjeta +" Reais o valor final da conta será de: " + contaFinal + " Reais");
+
