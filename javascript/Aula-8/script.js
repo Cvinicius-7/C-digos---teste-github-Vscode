bb80cafe4e5aca0bbc992ebe94ed1ea4fe3e3ede
Carlos Vinícius Pereira <p.carlosvini@gmail.com>
2024-02-27 22:31:21 -0300

    Change/update resolution scheme

--- javascript/Aula-8/script.js
+++ javascript/Aula-8/script.js
@@ -1,7 +1,10 @@
+//entrada
 let idade = 45;
+let msg = "O funcionário é menor de idade(" + idade + ")";
 
+//processamento
 if (idade >= 18) {
-  alert("o funcionário é maior de idade(" + idade + ")");
-} else {
-  alert("o funcionário é menor de idade(" + idade + ")");
+  msg = "O funcionário é maior de idade(" + idade + ")";
 }
+//saída
+alert(msg);
