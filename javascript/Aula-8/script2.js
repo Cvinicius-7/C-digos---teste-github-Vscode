0631f66e42c86d2adcd5df2b89316c58807cf1d2
Carlos Vinícius Pereira <p.carlosvini@gmail.com>
2024-02-27 22:54:36 -0300

    add first conditional scheme

---
+++ javascript/Aula-8/script2.js
@@ -0,0 +1,18 @@
+let = lado1 = 10;
+let = lado2 = 10;
+let = lado3 = 10;
+
+//tabela verdade
+// && - and
+// || - or
+// ! - not
+
+
+//condição
+if(lado1 == lado2 && lado2 == lado3){
+alert("Triângulo Equilátero");
+}
+//condição
+alert("Triângulo Isósceles");
+//condição
+alert("Triângulo Escaleno");
