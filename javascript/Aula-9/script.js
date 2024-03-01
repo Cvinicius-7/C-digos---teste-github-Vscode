4203ec0f559dd1b6ef6d87b4e9f8392b44603ae8
Carlos Vinícius Pereira <p.carlosvini@gmail.com>
2024-03-01 16:53:30 -0300

    Add another prompt validation

--- javascript/Aula-9/script.js
+++ javascript/Aula-9/script.js
@@ -1,17 +1,21 @@
 let letra = prompt("Informe uma Letra:").toLowerCase();
 
-if (letra >= "a" && letra <= "z") {
+if (letra.length == 1) {
+  if (letra >= "a" && letra <= "z") {
     if (
-        letra == "a" ||
-        letra == "e" ||
-        letra == "i" ||
-        letra == "o" ||
-        letra == "u"
+      letra == "a" ||
+      letra == "e" ||
+      letra == "i" ||
+      letra == "o" ||
+      letra == "u"
     ) {
-        alert("É uma vogal");
+      alert("É uma vogal");
     } else {
-        alert("É uma consoante ");
+      alert("É uma consoante ");
     }
-} else {
+  } else {
     alert("Não é uma letra");
+  }
+} else {
+  alert("Digite uma única letra do alfabeto, por favor!");
 }
