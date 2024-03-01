1496193dea025fccbbf549854b6066da7b1daefb
Carlos Vinícius Pereira <p.carlosvini@gmail.com>
2024-03-01 17:06:13 -0300

    create second scrupt scheme

---
+++ javascript/Aula-9/script2.js
@@ -0,0 +1,12 @@
+const idadeMin = 12;
+const alturaMin = 1.3;
+
+let idade = 45;
+let altura = 1.8;
+
+//condição 1
+if(idade >= idadeMin && altura >= alturaMin){
+    alert("A criança esta liberada para o brinquedo");
+}else{
+    alert("A criança não esta liberada para o brinquedo");      
+}
