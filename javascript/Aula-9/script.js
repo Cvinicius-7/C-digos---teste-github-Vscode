f2a3df0e7228daa19ca64360fec488371e96d31d
Carlos Vinícius Pereira <p.carlosvini@gmail.com>
2024-03-01 16:43:52 -0300

    Add input validation and format document

--- javascript/Aula-9/script.js
+++ javascript/Aula-9/script.js
@@ -1,9 +1,17 @@
-
-
 let letra = prompt("Informe uma Letra:").toLowerCase();
 
-if(letra=="a" || letra=="e" || letra=="i" || letra=="o" || letra=="u"){
-alert("É uma vogal");
-}else{
-alert("É uma consoante ");
+if (letra >= "a" && letra <= "z") {
+    if (
+        letra == "a" ||
+        letra == "e" ||
+        letra == "i" ||
+        letra == "o" ||
+        letra == "u"
+    ) {
+        alert("É uma vogal");
+    } else {
+        alert("É uma consoante ");
+    }
+} else {
+    alert("Não é uma letra");
 }
