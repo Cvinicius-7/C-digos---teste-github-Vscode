dbca8bac45716198ef08980cc2a5824c414c6030
Carlos Vinícius Pereira <p.carlosvini@gmail.com>
2024-03-01 17:17:06 -0300

    modify input prompt and add if else scheme

--- javascript/Aula-9/script2.js
+++ javascript/Aula-9/script2.js
@@ -1,12 +1,12 @@
 const idadeMin = 12;
 const alturaMin = 1.3;
 
-let idade = 45;
-let altura = 1.8;
+let idade =  parseInt(prompt("Informe a idade:"));
+let altura = parseFloat(prompt("Informe a altura:"));
 
 //condição 1
-if(idade >= idadeMin && altura >= alturaMin){
-    alert("A criança esta liberada para o brinquedo");
-}else{
-    alert("A criança não esta liberada para o brinquedo");      
+if (idade >= idadeMin && altura >= alturaMin) {
+  alert("A criança esta liberada para o brinquedo");
+} else {
+  alert("A criança não esta liberada para o brinquedo");
 }
