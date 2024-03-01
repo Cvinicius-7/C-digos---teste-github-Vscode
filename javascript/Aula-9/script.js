17d04725d00e0534a35a8c0660c04befc12ada92
Carlos Vinícius Pereira <p.carlosvini@gmail.com>
2024-03-01 16:35:57 -0300

    Update if scheme and add prompt input

--- javascript/Aula-9/script.js
+++ javascript/Aula-9/script.js
@@ -1,8 +1,8 @@
 
 
-let letra = "a";
+let letra = prompt("Informe uma Letra:").toLowerCase();
 
-if(letra=="a"){
+if(letra=="a" || letra=="e" || letra=="i" || letra=="o" || letra=="u"){
 alert("É uma vogal");
 }else{
 alert("É uma consoante ");
