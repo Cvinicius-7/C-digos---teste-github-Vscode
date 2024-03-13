e2f1ff3787390aade11c1ea41ae61b86ac37e15c
Carlos Vinícius Pereira <p.carlosvini@gmail.com>
2024-03-12 22:05:33 -0300

    full implement first feature

--- javascript/Aula-12/script.js
+++ javascript/Aula-12/script.js
@@ -1,5 +1,5 @@
-let nomeAluno = "João";
-let nomeDisciplina = "Programação Web";
-let qtdProvas = 3;
+let nomeAluno = prompt("Digite o nome do aluno: ");
+let nomeDisciplina = prompt("Digite qual disciplina:");
+let qtdProvas = parseInt(prompt("Digite a quantidade de provas:"));
 
 alert(nomeAluno + "..." + nomeDisciplina + "..." + qtdProvas);
