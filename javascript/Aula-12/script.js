ddd124e72da510b10bda339bf31ae3db3568053f
Carlos Vinícius Pereira <p.carlosvini@gmail.com>
2024-03-13 00:18:35 -0300

    add first for scope

--- javascript/Aula-12/script.js
+++ javascript/Aula-12/script.js
@@ -2,4 +2,14 @@ let nomeAluno = prompt("Digite o nome do aluno: ");
 let nomeDisciplina = prompt("Digite qual disciplina:");
 let qtdProvas = parseInt(prompt("Digite a quantidade de provas:"));
 
+let notas = [];
+
+for(let i = 0; i<qtdProvas; i++){
+    notas.push(parseFloat(prompt("Digite a nota da prova")));
+
+ }
+ for(let i; i<notas.length; i++){
+     alert(notas[i]);
+ }
+
 alert(nomeAluno + "..." + nomeDisciplina + "..." + qtdProvas);
