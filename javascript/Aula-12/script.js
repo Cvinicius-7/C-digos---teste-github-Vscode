4fd444c7329b0b8e12eea9b6c7fdf836f2256bd5
Carlos Vinícius Pereira <p.carlosvini@gmail.com>
2024-03-13 00:46:01 -0300

    Create principal algorithim

--- javascript/Aula-12/script.js
+++ javascript/Aula-12/script.js
@@ -1,15 +1,23 @@
-let nomeAluno = prompt("Digite o nome do aluno: ");
+let nomeAluno = prompt("Digite o nome do aluno:");
 let nomeDisciplina = prompt("Digite qual disciplina:");
 let qtdProvas = parseInt(prompt("Digite a quantidade de provas:"));
 
 let notas = [];
 
-for(let i = 0; i<qtdProvas; i++){
-    notas.push(parseFloat(prompt("Digite a nota da prova")));
+for (let i = 0; i < qtdProvas; i++) {
 
- }
- for(let i; i<notas.length; i++){
-     alert(notas[i]);
- }
+  notas.push(parseFloat(prompt("Digite a nota da prova")));
+}
 
-alert(nomeAluno + "..." + nomeDisciplina + "..." + qtdProvas);
+let media = 0;
+let soma = 0;
+
+for (let i; i < notas.length; i++) {
+
+  soma = soma + notas[i];
+
+}
+
+media = soma / notas.length;
+
+alert(nomeAluno + "-" + nomeDisciplina + "-" + qtdProvas);
