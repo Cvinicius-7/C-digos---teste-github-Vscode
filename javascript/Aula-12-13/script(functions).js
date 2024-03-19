8cc14bc87e91adfbf3b388cf8106b25d403f1118
Carlos Vinícius Pereira <p.carlosvini@gmail.com>
2024-03-18 23:29:59 -0300

    Fix and format

--- javascript/Aula-12-13/script(functions).js
+++ javascript/Aula-12-13/script(functions).js
@@ -2,16 +2,15 @@ let nomeAluno = prompt("Digite o nome do aluno:");
 let nomeDisciplina = prompt("Digite qual disciplina:");
 let qtdProvas = parseInt(prompt("Digite a quantidade de provas:"));
 
+function receberNotas(aQDProvas) {
+  let notas = [];
 
+  for (let i = 0; i < aQDProvas; i++) {
+    let nota = parseFloat(prompt("Digite a nota da prova"));
 
-function receberNotas(aQDProvas){
-    let notas = [];
-
-for (let i = 0; i < aQDProvas; i++) {
-  let nota = parseFloat(prompt("Digite a nota da prova"));
-
-  notas.push(nota);
-}
+    notas.push(nota);
+  }
+  return notas;
 }
 
 function calcularMedia(asNotas) {
@@ -35,7 +34,7 @@ function obterSituacao(aMedia) {
 
   return "reprovado";
 }
-let notas = receberNotas(qteProva);
+let notas = receberNotas(qtdProvas);
 let media = calcularMedia(notas);
 let situacao = obterSituacao(media);
 
