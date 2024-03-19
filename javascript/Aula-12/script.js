7b53e4154adb3ea3d541a49738ba5c4a19bf865f
Carlos Vinícius Pereira <p.carlosvini@gmail.com>
2024-03-18 22:20:56 -0300

    Add First Comparation Method

--- javascript/Aula-12/script.js
+++ javascript/Aula-12/script.js
@@ -5,7 +5,6 @@ let qtdProvas = parseInt(prompt("Digite a quantidade de provas:"));
 let notas = [];
 
 for (let i = 0; i < qtdProvas; i++) {
-
   notas.push(parseFloat(prompt("Digite a nota da prova")));
 }
 
@@ -13,11 +12,18 @@ let media = 0;
 let soma = 0;
 
 for (let i; i < notas.length; i++) {
-
   soma = soma + notas[i];
-
 }
 
 media = soma / notas.length;
 
-alert(nomeAluno + "-" + nomeDisciplina + "-" + media.toFixed(2));
+const notaAprovacao = 7;
+let situacao = "Reprovado";
+
+if (media >= notaAprovacao) {
+  situacao = "Aprovado";
+}
+
+alert(
+  nomeAluno + "-" + nomeDisciplina + "-" + media.toFixed(2) + "-" + situacao
+);
