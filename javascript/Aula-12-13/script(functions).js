17594a085a9b54d96cab81d0d16f1084a3852688
Carlos Vinícius Pereira <p.carlosvini@gmail.com>
2024-03-18 23:14:18 -0300

    create second function

--- javascript/Aula-12-13/script(functions).js
+++ javascript/Aula-12-13/script(functions).js
@@ -1,4 +1,18 @@
 
+
+function calcularMedia(asNotas) {
+  let media = 0;
+  let soma = 0;
+
+  for (let i = 0; i < asNotas.length; i++) {
+    soma = soma + asNotas[i];
+  }
+
+  media = soma / asNotas.length;
+
+  return media;
+}
+
 function obterSituacao(aMedia) {
   const notaAprovacao = 7;
   if (aMedia >= notaAprovacao) {
@@ -7,9 +21,10 @@ function obterSituacao(aMedia) {
 
   return "reprovado";
 }
+
+let media = calcularMedia(notas);
 let situacao = obterSituacao(media);
 
 alert(
-    nomeAluno + "-" + nomeDisciplina + "-" + media.toFixed(2) + "-" + situacao
-  );
-   
+  nomeAluno + "-" + nomeDisciplina + "-" + media.toFixed(2) + "-" + situacao
+);
