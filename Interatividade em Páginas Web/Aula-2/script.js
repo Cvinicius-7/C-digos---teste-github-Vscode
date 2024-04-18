e663dd7fab37427f92241ea0268a9e431db9b096
Carlos Vinícius Pereira <157232610+Cvinicius-7@users.noreply.github.com>
2024-04-18 23:12:25 +0000

    fix css and add script method

--- Interatividade em Páginas Web/Aula-2/script.js
+++ Interatividade em Páginas Web/Aula-2/script.js
@@ -9,4 +9,7 @@ function Person(first,last,id){
 let personArray = [new Person("John","Doe",1),new Person("Jane","Doe",2),new Person("jack","Doe",3),new Person("lewis","Doe",4),new Person("alexia","Doe",5)]
 
 let conteudo_tabela = "<tr><th>Primeiro Nome</th><th>Ultimo Nome</th><th>Identificador</th><th>Nome Completo</th></tr>"
-document.getElementById("table_peroson").innerHTML = conteudo_tabela
+for (let i in personArray) {
+    conteudo_tabela += "<tr><td>" + personArray[i].firstName + "</td><td>" + personArray[i].lastName + "</td><td>" + personArray[i].id + "</td><td>" + personArray[i].fullName() + "</td></tr>"
+  }
+document.getElementById("table_person").innerHTML = conteudo_tabela;
