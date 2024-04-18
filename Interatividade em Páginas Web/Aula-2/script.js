b9c9cd0bd8dcf25c622dbab31cf4f060e458a0dc
Carlos Vinícius Pereira <157232610+Cvinicius-7@users.noreply.github.com>
2024-04-18 23:01:14 +0000

    add array header script

--- Interatividade em Páginas Web/Aula-2/script.js
+++ Interatividade em Páginas Web/Aula-2/script.js
@@ -2,8 +2,11 @@ function Person(first,last,id){
     this.firstName = first;
     this.lastName = last;
     this.id = id;
-    this.fullname = function(){
+    this.fullName = function(){
         return this.firstName + " " +this.lastName;
-    }
+    };
 }
-;
+let personArray = [new Person("John","Doe",1),new Person("Jane","Doe",2),new Person("jack","Doe",3),new Person("lewis","Doe",4),new Person("alexia","Doe",5)]
+
+let conteudo_tabela = "<tr><th>Primeiro Nome</th><th>Ultimo Nome</th><th>Identificador</th><th>Nome Completo</th></tr>"
+document.getElementById("table_peroson").innerHTML = conteudo_tabela
