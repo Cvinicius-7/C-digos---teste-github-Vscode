9a4017aa09afb183128d3867f0fdd0fd2a516365
Carlos Vinícius Pereira <157232610+Cvinicius-7@users.noreply.github.com>
2024-04-19 00:10:24 +0000

    create instructions

---
+++ javascript 2/script.js
@@ -0,0 +1,9 @@
+//cadastramento de jogadores
+
+//informar quantos jogadores serão cadastrados
+
+//goleiro,lateral,meia,atacante : previamente preenchido num vetor
+
+//exibir nome e posiçaõ do jogador
+
+//um vetor para os nomes e outro para as posições
