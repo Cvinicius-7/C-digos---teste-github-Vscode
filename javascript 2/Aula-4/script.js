fa444cdee0aeb6bb12c428e38b228589810f73f6
Carlos Vinicius Pereira <p.carlosvini@gmail.com>
2024-05-07 22:12:36 -0300

    add input inplemetation and begin table organization function

--- javascript 2/Aula-4/script.js
+++ javascript 2/Aula-4/script.js
@@ -1,21 +1,33 @@
 let disciplinas = [];
 
-function incluir(disciplina, professor){
+function inclusao(){
+    let nome = document.getElementById("nome").value;
+    let professor = document.getElementById("professor").value;
+    let curso = document.getElementById("curso").value;
+
+    incluir(nome, professor, curso);
+}
+
+function incluir(disciplina, professor, curso){
 
     let disciplina = {
         id: disciplinas.length +1,
         nome: nome,
-        professor: professor
+        professor: professor,
+        curso: curso
     };
 
     disciplinas.push(disciplina);
 }
 function obterLista(){
     disciplinas.forEach(disciplina => {
-        alert (`${disciplina.id} | ${disciplina.nome} | ${disciplina.professor}`);
+        alert (`${disciplina.id} | ${disciplina.nome} | ${disciplina.professor} | ${disciplina.curso}`);
     });
 }
 
-incluir("teste");
+function atualizarTable(){
+    let tbody = document.querySelector("#main-table tbody");
+}
+
 
 obterLista();
