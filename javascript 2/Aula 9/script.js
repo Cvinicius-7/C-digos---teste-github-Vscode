061a9a720bd12a30baea35e88fdffc5002629cfb
Carlos Vinícius Pereira <157232610+Cvinicius-7@users.noreply.github.com>
2024-05-22 00:28:17 +0000

    create input validation function

--- javascript 2/Aula 9/script.js
+++ javascript 2/Aula 9/script.js
@@ -1,11 +1,22 @@
 //registrar uma venda
+function validarCamposObrigatorios(produto, quantidade, preco, data){
+    if (produto && quantidade && preco && data){
+        return true;
+    }else{
+        return false;
+    }
+
+}
+
 function registrarVenda() {
     let produto = "refrigerante";
     let quantidade = 2;
     let preco = 6;
     let data = "2023-06-01";
 
-    if (produto && quantidade && preco && data) {
+    let validaCampos = validarCamposObrigatorios(produto, quantidade, preco, data );
+
+        if(validaCampos){
         alert("Venda registrada !" + produto + ", " + quantidade + "unidades , R$" + preco + "por unidade em, " + data);
 
     } else {
