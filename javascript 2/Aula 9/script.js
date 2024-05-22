e3a2a4af6d65884dc473d59a1af5f89c94df1041
Carlos Vinícius Pereira <157232610+Cvinicius-7@users.noreply.github.com>
2024-05-22 00:17:45 +0000

    first script scheme

---
+++ javascript 2/Aula 9/script.js
@@ -0,0 +1,13 @@
+//registrar uma venda
+
+let produto = "refrigerante";
+let quantidade = 2;
+let preco = 6;
+let data = "2023-06-01";
+
+if(produto && quantidade && preco && data){
+    alert("Venda registrada !" + produto + ", " + quantidade + "unidades , R$" + preco + "por unidade em, "+ data );
+    
+}else{
+    alert("problemas na venda!")
+}
