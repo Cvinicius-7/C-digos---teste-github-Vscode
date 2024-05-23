51843f18d86cc44c953be272336d9cef0e5ad95e
Carlos Vinícius Pereira <157232610+Cvinicius-7@users.noreply.github.com>
2024-05-23 00:27:59 +0000

    add count vote function

--- Interatividade em Páginas Web/Aula-7-8/script.js
+++ Interatividade em Páginas Web/Aula-7-8/script.js
@@ -1,15 +1,14 @@
 var a_nomes = [];
 
-function increment_vote(){
-
-let count = parseInt(item.children[0].innerText,10);
-count++
-item.children[0].innerText = count
+function increment_vote(item) {
+    let count = parseInt(item.children[0].innerText, 10);
+    count++;
+    item.children[0].innerText = count;
 }
-function adicionar(){
 
+function adicionar() {
     var nome = document.getElementById("input").value;
-    if(!a_nomes.includes(nome)){
+    if (!a_nomes.includes(nome)) {
         a_nomes.push(nome);
         var item = document.createElement("li");
         item.innerHTML = nome + " <span>0</span>";
