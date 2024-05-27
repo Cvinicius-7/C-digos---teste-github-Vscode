eb34dc3dee6f0ad9ec94281627807cf2a2dbff22
Carlos Vinícius Pereira <157232610+Cvinicius-7@users.noreply.github.com>
2024-05-27 23:15:26 +0000

    create output list scheme

--- javascript 2/Aula 9/script.js
+++ javascript 2/Aula 9/script.js
@@ -1,28 +1,25 @@
 
 function validarCamposObrigatorios(produto, quantidade, preco, data) {
-
-    //return (produto && quantidade && preco && data) ? true: false;
-
-    return produto && quantidade != null && preco != null && data;
-
+    return produto && quantidade !== '' && preco !== '' && data;
 }
+
 function validarQuantidadeProdutos(quantidade) {
     return quantidade > 0;
-
 }
+
 function validarPrecoUnitario(preco) {
     return preco > 0;
 }
+
 function obterDataTransacao() {
     return new Date().toLocaleString();
 }
+
 function gerarNumeroTransacao(){
     return Math.floor(Math.random() * 100000);
 }
+
 function registrarVenda(produto, quantidade, preco) {
-    let produto;
-    let quantidade;
-    let preco;
     let data = obterDataTransacao();
     let numeroTransacao = gerarNumeroTransacao();
 
@@ -33,28 +30,27 @@ function registrarVenda(produto, quantidade, preco) {
     if (validaCampos) {
         if (validaProdutos) {
             if (validaPreco) {
-                return alert("("+ numeroTransacao+") Venda registrada !" + produto + ", " + quantidade + "unidades , R$" + preco + "por unidade em, " + data);
+                return "(" + numeroTransacao + ") Venda registrada ! " + produto + ", " + quantidade + " unidades, R$" + preco + " por unidade em, " + data;
             } else {
-                alert("Preco unitario invalido !")
+                alert("Preço unitário inválido!");
             }
         } else {
-            alert("Problema na validacao da quantidade de produtos !");
+            alert("Problema na validação da quantidade de produtos!");
         }
     } else {
-        alert("problemas na venda!");
+        alert("Problemas na venda!");
     }
 }
-function vender(){
 
+function vender(){
     const produto = document.getElementById("produto").value;
     const quantidade = document.getElementById("quantidade").value;
     const preco = document.getElementById("preco").value;
-
 
     let mensagem = registrarVenda(produto,quantidade,preco);
 
     const historicoVendas = document.getElementById("historicoVendas");
     const li = document.createElement("li");
-    li.textContent = "mensagem";
+    li.textContent = mensagem;
     historicoVendas.appendChild(li);
 }
